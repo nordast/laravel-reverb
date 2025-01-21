a96f7d864e654ce60def8f7cc2f7e15339fd71a9
nordast <nordast@gmail.com>
2025-01-21 13:46:16 +0200

    install reverb

---
+++ app/Events/SwitchFlipped.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SwitchFlipped implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $toggleSwitch;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($toggleSwitch)
+    {
+        $this->toggleSwitch = $toggleSwitch;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('switch'),
+        ];
+    }
+
+    public function broadcastWith(): array
+    {
+        return ['toggleSwitch' => $this->toggleSwitch];
+    }
+}
--- resources/views/livewire/toggle.blade.php
+++ resources/views/livewire/toggle.blade.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Events\SwitchFlipped;
+use Livewire\Attributes\On;
 use Livewire\Volt\Component;
 
 new class extends Component {
 
     public $toggleSwitch = false;
+
+    public function mount()
+    {
+        $this->toggleSwitch = Cache::get('toggleSwitch', false);
+    }
+
+    public function flipSwitch()
+    {
+        Cache::forever('toggleSwitch', $this->toggleSwitch);
+        broadcast(new SwitchFlipped($this->toggleSwitch))->toOthers();
+    }
+
+    #[On('echo:switch, SwitchFlipped')]
+    public function registerSwitchFlipped($payload)
+    {
+        $this->toggleSwitch = $payload['toggleSwitch'];
+        Cache::forever('toggleSwitch', $this->toggleSwitch);
+    }
 
 }; ?>
 
@@ -14,9 +34,11 @@ new class extends Component {
     <div class="flex items-center justify-center min-h-screen">
         <label for="toggleSwitch" class="flex items-center cursor-pointer">
             <div class="relative">
-                <input type="checkbox" id="toggleSwitch" class="sr-only" x-model="localToggle">
+                <input type="checkbox" id="toggleSwitch" class="sr-only" x-model="localToggle"
+                       x-on:change="$wire.flipSwitch()">
                 <div class="block h-8 bg-gray-600 rounded-full w-14"></div>
-                <div class="absolute w-6 h-6 transition-transform duration-200 rounded-full left-1 top-1" x-bind:class="localToggle ? 'translate-x-full bg-green-400' : 'bg-white'">
+                <div class="absolute w-6 h-6 transition-transform duration-200 rounded-full left-1 top-1"
+                     x-bind:class="localToggle ? 'translate-x-full bg-green-400' : 'bg-white'">
                 </div>
             </div>
         </label>
